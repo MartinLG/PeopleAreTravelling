45dc27045485e60701ad924bfb584e6f62f80b12
MartinLG <martin.leguillou@ingesup.com>
2013-12-19 12:33:26 +0100

    User Login fonctionne, registration non, BDD ok

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PaT\MaquetteBundle\PaTMaquetteBundle(),
+            new PaT\UserBundle\PaTUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
