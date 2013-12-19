b4d00e67eb7661041c04f473b8ed246ae28524d9
MartinLG <martin.leguillou@ingesup.com>
2013-12-19 13:41:36 +0100

    User Register marche, a integrer dans le pop

---
+++ src/PaT/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PaT\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // add your custom field
+        $builder->add('country');
+    }
+
+    public function getName()
+    {
+        return 'PaT_user_registration';
+    }
+}
