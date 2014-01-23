6ac032638bd92d511676b842abb5cdc10de425b6
MartinLG <martin.leguillou@ingesup.com>
2014-01-23 23:28:10 +0100

    UML démarré, ChangeStatus fini

--- src/PaT/UserBundle/Controller/ChangeStatusController.php
+++ src/PaT/UserBundle/Controller/ChangeStatusController.php
@@ -4,12 +4,36 @@ namespace PaT\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use PaT\UserBundle\Entity\User;
+use PaT\UserBundle\Form\StatusType;
 
 
 class ChangeStatusController extends Controller
 {
 	public function indexAction()
     {
-        return $this->render('PaTUserBundle:ChangeStatus:index.html.twig');  //affiche la vue correspondante
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        // on cree le formulair en fonction de traveleditetype 
+	    $form = $this->createForm(new StatusType, $user);
+	    $request = $this->get('request');
+
+	    if($request->getMethod() == 'POST')
+	    {
+	       $form->bind($request);
+
+	       if($form->isValid())
+	       {
+
+	          $em = $this->getDoctrine()->getManager();
+	          $em->persist($user);
+	          $em->flush();
+
+	          $this->get('session')->getFlashBag()->add('info', 'Your status has been edited successfully');
+
+	          return $this->redirect($this->generateUrl('Account'));
+	        }
+	      }
+
+	    return $this->render('PaTUserBundle:ChangeStatus:index.html.twig', array('form' => $form->createView()));
     }
 }
---
+++ src/PaT/UserBundle/Form/StatusType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PaT\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class StatusType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('status', 'text')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PaT\UserBundle\Entity\User',
+            'csrf_protection' => false,
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pat_userbundle_status';
+    }
+}
