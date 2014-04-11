c3d5d2af9dd6e53b50a34c07a06cb38c5c414a8f
MartinLG <martin.leguillou@y-nov.com>
2014-04-11 18:45:15 +0200

    commit intermédiaire

--- src/PaT/AlbumBundle/Controller/AlbumController.php
+++ src/PaT/AlbumBundle/Controller/AlbumController.php
@@ -6,10 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AlbumController extends Controller
 {
-    public function viewAction($album)
+    public function viewAction($idalbum)
     {
         $em = $this->getDoctrine()->getManager();
-        $pics = $em->getRepository('PaTImageBundle:Pictures')->findByAlbum($album);
-        return $this->render('PaTAlbumBundle:Album:index.html.twig', array('pics' => $pics, 'album' => $album));
+        $album = $em->getRepository('PaTAlbumBundle:Album')->find($idalbum);
+        $pics = $em->getRepository('PaTImageBundle:Pictures')->findBy(array('album' => $idalbum));
+        return $this->render('PaTAlbumBundle:Album:index.html.twig', array('Pics' => $pics, 'Album' => $album));
     }
 }
